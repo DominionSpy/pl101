961180b0a02a269f721e3c12747e36c58a76c738
Matthew Wilson <dominionspy@hotmail.com>
2012-04-16 18:50:51 +0100

    Converted compiler to iterative and cleaned up superfluous methods

--- mus/compiler.js
+++ mus/compiler.js
@@ -1,47 +1,56 @@
-var endTime = function (time, expr) {
-    if (expr.tag == "note" || expr.tag == "rest") return time + expr.dur;
-    if (expr.tag == "repeat") return time + (endTime(0, expr.section) * expr.count);
-    if (expr.tag == "par") return Math.max(endTime(time, expr.left), endTime(time, expr.right));
-    return endTime(endTime(time, expr.left), expr.right);
-};
-
-var convertPitch = function(name) {
-    var letterPitches = { c: 0, d: 2, e: 4, f: 5, g: 7, a: 9, b: 11 };
-    return 12 + (12 * name.charAt(1)) + letterPitches[name.charAt(0)];
-};
-
-var getNote = function (time, expr) {
-    var note = { tag: 'note' };
-    note.start = time;
-    note.dur = expr.dur;
-    if (expr.tag == "note") {
-        note.pitch = convertPitch(expr.pitch);
+var compile = function (musexpr)
+{
+  var stack = [musexpr];
+  var notes = [];
+  var times = [];
+  var time = 0;
+  var endtime = 0;
+  while (stack.length !== 0)
+  {
+    var curr = stack.pop();
+    if (curr.tag === "rest")
+    {
+      notes.push({ tag: "note", dur: curr.dur, start: time });
+      time += curr.dur;
     }
-    return note;
-};
-
-var getNotes = function (time, expr) {
-    if (expr.tag == "note" || expr.tag == "rest") {
-        return [getNote(time, expr)];
+    else if (curr.tag === "note")
+    {
+      var midi = 12 + {c:0,d:2,e:4,f:5,g:7,a:9,b:11}[curr.pitch.charAt(0)] + (12 * curr.pitch.charAt(1));
+      notes.push({ tag: "note", pitch: midi, dur: curr.dur, start: time });
+      time += curr.dur;
     }
-    if (expr.tag == "repeat") {
-        var notes = [];
-        var end = time;
-        for (var i = 0; i < expr.count; i++) {
-            notes = notes.concat(getNotes(end, expr.section));
-            end = endTime(end, expr.section);
-        }
-        return notes;
+    else if (curr.tag === "repeat")
+    {
+      for (var i = 0; i < curr.count; i++)
+      {
+        stack.push(curr.section);
+      }
     }
-    if (expr.tag == "par") {
-        return getNotes(time, expr.left).concat(getNotes(time, expr.right));
+    else if (curr.tag === "par")
+    {
+      times.push(time);
+      stack.push({ tag: "endpar" });
+      stack.push(curr.right);
+      stack.push({ tag: "reset" });
+      stack.push(curr.left);
     }
-    return getNotes(time, expr.left).concat(getNotes(endTime(time, expr.left), expr.right));
-};
-
-var compile = function (musexpr) {
-    var noteexpr = getNotes(0, musexpr);
-    return noteexpr;
+    else if (curr.tag === "seq")
+    {
+      stack.push(curr.right);
+      stack.push(curr.left);
+    }
+    else if (curr.tag === "reset")
+    {
+      endtime = time;
+      time = times.pop();
+    }
+    else if (curr.tag === "endpar")
+    {
+      time = Math.max(time, endtime);
+      endtime = 0;
+    }
+  }
+  return notes;
 };
 
 var melody_mus = 
@@ -56,7 +65,10 @@ var melody_mus =
       right:
        { tag: 'par',
          left: { tag: 'note', pitch: 'c4', dur: 500 },
-         right: { tag: 'note', pitch: 'd4', dur: 500 } } };
+         right:
+         { tag: 'seq',
+           left: { tag: 'note', pitch: 'd4', dur: 500 },
+           right: { tag: 'note', pitch: 'f3', dur: 250 } } } };
 
 console.log(melody_mus);
 console.log(compile(melody_mus));
