5cedf42b848c0fdb4e0ef014dbddb03a6fca6425
Matthew Wilson <dominionspy@hotmail.com>
2012-05-05 21:01:44 +0100

    Lesson 4 part 1

--- scheem/interpret.js
+++ scheem/interpret.js
@@ -5,18 +5,28 @@ var evalScheem = function (expr, env) {
     }
     // Strings are variable references
     if (typeof expr === 'string') {
+        if (!env[expr]) throw new Error("Cannot reference an undefined variable");
         return env[expr];
     }
     if (expr === 'error') throw('Error');
     // Look at head of list for operation
     switch (expr[0]) {
         case 'quote':
+            if (expr.length != 2) throw new Error("Unexpected number of arguments");
             return expr[1];
         case 'define':
+            if (expr.length != 3) throw new Error("Unexpected number of arguments");
+            if (typeof expr[1] !== 'string') throw new Error("Expecting variable name");
+            env[expr[1]] = evalScheem(expr[2], env);
+            return 0;
         case 'set!':
+            if (expr.length != 3) throw new Error("Unexpected number of arguments");
+            if (typeof expr[1] !== 'string') throw new Error("Expecting variable name");
+            if (!env[expr[1]]) throw new Error("Cannot set an undefined variable");
             env[expr[1]] = evalScheem(expr[2], env);
             return 0;
         case 'begin':
+            if (expr.length < 2) throw new Error("Unexpected number of arguments");
             var retval;
             for (var i = 1; i < expr.length; i++)
             {
@@ -24,40 +34,58 @@ var evalScheem = function (expr, env) {
             }
             return retval;
         case '+':
-            return evalScheem(expr[1], env) +
-                   evalScheem(expr[2], env);
+            if (expr.length != 3) throw new Error("Unexpected number of arguments");
+            var arg1 = evalScheem(expr[1], env);
+            var arg2 = evalScheem(expr[2], env);
+            if (typeof arg1 !== 'number' || typeof arg2 !== 'number') throw new Error("Arguments must evaluate to numbers");
+            return arg1 + arg2;
         case '-':
-            return evalScheem(expr[1], env) -
-                   evalScheem(expr[2], env);
+            if (expr.length != 3) throw new Error("Unexpected number of arguments");
+            var arg1 = evalScheem(expr[1], env);
+            var arg2 = evalScheem(expr[2], env);
+            if (typeof arg1 !== 'number' || typeof arg2 !== 'number') throw new Error("Arguments must evaluate to numbers");
+            return arg1 - arg2;
         case '*':
-            return evalScheem(expr[1], env) *
-                   evalScheem(expr[2], env);
+            if (expr.length != 3) throw new Error("Unexpected number of arguments");
+            var arg1 = evalScheem(expr[1], env);
+            var arg2 = evalScheem(expr[2], env);
+            if (typeof arg1 !== 'number' || typeof arg2 !== 'number') throw new Error("Arguments must evaluate to numbers");
+            return arg1 * arg2;
         case '/':
-            return evalScheem(expr[1], env) /
-                   evalScheem(expr[2], env);
+            if (expr.length != 3) throw new Error("Unexpected number of arguments");
+            var arg1 = evalScheem(expr[1], env);
+            var arg2 = evalScheem(expr[2], env);
+            if (typeof arg1 !== 'number' || typeof arg2 !== 'number') throw new Error("Arguments must evaluate to numbers");
+            return arg1 / arg2;
         case '=':
-            var eq =
-                (evalScheem(expr[1], env) ===
-                 evalScheem(expr[2], env));
+            if (expr.length != 3) throw new Error("Unexpected number of arguments");
+            var arg1 = evalScheem(expr[1], env);
+            var arg2 = evalScheem(expr[2], env);
+            var eq = arg1 === arg2;
             if (eq) return '#t';
             return '#f';
         case '<':
-            var lt =
-                (evalScheem(expr[1], env) <
-                 evalScheem(expr[2], env));
+            if (expr.length != 3) throw new Error("Unexpected number of arguments");
+            var arg1 = evalScheem(expr[1], env);
+            var arg2 = evalScheem(expr[2], env);
+            if (typeof arg1 !== 'number' || typeof arg2 !== 'number') throw new Error("Arguments must evaluate to numbers");
+            var lt = arg1 < arg2;
             if (lt) return '#t';
             return '#f';
         case 'cons':
+            if (expr.length != 3) throw new Error("Unexpected number of arguments");
             return [evalScheem(expr[1], env)].concat(evalScheem(expr[2], env));
         case 'car':
+            if (expr.length != 2) throw new Error("Unexpected number of arguments");
             return evalScheem(expr[1], env)[0];
         case 'cdr':
+            if (expr.length != 2) throw new Error("Unexpected number of arguments");
             return evalScheem(expr[1], env).slice(1);
         case 'if':
-            if (evalScheem(expr[1], env) === '#t')
-            {
-                return evalScheem(expr[2], env);
-            }
-            return evalScheem(expr[3], env);
+            if (expr.length != 4) throw new Error("Unexpected number of arguments");
+            var cond = evalScheem(expr[1], env)
+            if  (cond === '#t') return evalScheem(expr[2], env);
+            else if (cond === '#f') return evalScheem(expr[3], env);
+            throw new Error("Condition must resolve to true or false");
     }
 };
